2b7fbc062281138b6275ed3cd87ca05ce414035c
Janaka A <contact@janaka.co.uk>
2013-12-24 01:00:58 +0000

    Fix some typos and add functions

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import time
 import picamera
 
-with picamera.Picamera() as camera:
-    camera.resolution = (1280, 720)
-    camera.start_preview()
-    camera.exposure_compensation = 2
-    camera.exposure_mode = 'spotlight'
-    camera.meter_mode = 'matrix'
-    camera.image_effect = 'gpen'
-    time.sleep(2)
-    camera.capture('foo.jpeg')
-    camera.stop_preview()
+
+
+
+def takePicture():
+
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        camera.start_preview()
+        camera.exposure_compensation = 2
+        camera.exposure_mode = 'spotlight'
+        camera.meter_mode = 'matrix'
+        camera.image_effect = 'gpen'
+        time.sleep(2)
+        camera.capture('foo.jpeg')
+        camera.stop_preview()
     
+def preview1():
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1280, 720)
+        camera.start_preview()
+        #camera.exposure_compensation = 2
+        #camera.exposure_mode = 'spotlight'
+        #camera.meter_mode = 'matrix'
+        #camera.image_effect = 'gpen'
+        time.sleep(10)
+        #camera.capture('foo.jpeg')
+        camera.stop_preview()
     
+preview1()
